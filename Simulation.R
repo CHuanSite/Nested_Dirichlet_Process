74503d6d091e018cf0d26c1bb94826ffa6b28b65
CHuanSite <hzchenhuan@gmail.com>
2019-04-03 21:12:04 -0400

    Add the simulation code

---
+++ Simulation.R
@@ -0,0 +1,21 @@
+## The weight for different distributions in the mixture
+w = list()
+w[[1]] = c(0.75, 0.25)
+w[[2]] = c(0.55, 0.45)
+w[[3]] = c(0.40, 0.30, 0.30)
+w[[4]] = c(0.39, 0.39, 0.29, 0.03)
+
+## The mean for different distirubtion in the mixture
+mu = list()
+mu[[1]] = c(0, 3.0)
+mu[[2]] = c(0, 3.0)
+mu[[3]] = c(0, -2.0, 2.0)
+mu[[4]] = c(0, -2.0, 2.0, 10.0)
+
+## The variance for different distribution in the mixture
+
+sigma = list()
+sigma[[1]] = c(1.0, 2.0)
+sigma[[2]] = c(1.0, 2.0)
+sigma[[3]] = c(1.0, 2.0, 2.0)
+sigma[[4]] = c(1.0, 2.0, 2.0, 1.0)
