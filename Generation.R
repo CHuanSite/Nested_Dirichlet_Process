d94e152004cbe925fc8120e2f16750adbf17996d
CHuanSite <hzchenhuan@gmail.com>
2019-04-03 21:21:37 -0400

    Change the name of simulation.R

--- Generation.R
+++ Generation.R
@@ -13,9 +13,18 @@ mu[[3]] = c(0, -2.0, 2.0)
 mu[[4]] = c(0, -2.0, 2.0, 10.0)
 
 ## The variance for different distribution in the mixture
-
 sigma = list()
 sigma[[1]] = c(1.0, 2.0)
 sigma[[2]] = c(1.0, 2.0)
 sigma[[3]] = c(1.0, 2.0, 2.0)
 sigma[[4]] = c(1.0, 2.0, 2.0, 1.0)
+
+## The number of samples and its nested samples
+J = 20
+N = 100
+
+## The density for the four mixtures
+p_density = list()
+p_density[[1]] = w[[1]][1] * dnorm(seq(-10, 10, 0.1), mu[[1]][1], sqrt(sigma[[1]][1])) + 
+  w[[1]][2] * dnorm(seq(-10, 10, 0.1), mu[[1]][2], sqrt(sigma[[1]][2]))
+
